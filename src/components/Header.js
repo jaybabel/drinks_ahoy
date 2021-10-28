40bf4ec4501ee711d46426616d0fb44467b614e5
jaybabel <BabelJayE@JohnDeere.com>
2021-10-28 10:21:41 -0500

    added link to Alphabetical Search in app header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,17 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 // import "./Header.css"  
 class Header extends Component {
-    
+
   render() {
     return (
       <header className="header">
         <h1 className="App-title">Drinks Ahoy!!</h1>
+        <Link to="/alphabetical" >
+          <div className="alphabeticalLink">
+            <h4>Alphabetical Search</h4>
+          </div>
+        </Link>
       </header>
     );
   }

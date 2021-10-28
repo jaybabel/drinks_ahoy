bc5f7edaa018e004ecd196f84e42f1c0c7d4944b
Bob DeBlaey <deblaeyboba@johndeere.com>
2021-10-28 16:04:29 -0500

    basic search api and page setup

---
+++ src/components/Search.js
@@ -0,0 +1,24 @@
+import { render } from "@testing-library/react";
+import React from "react";
+import './Header.css';
+
+const Search = (props) => {
+
+    return(
+        console.log(props.drinkSearch),
+
+        <div>
+            <h1>Search Results</h1>
+
+
+
+
+
+
+
+        </div>
+
+    )
+}
+
+export default Search;

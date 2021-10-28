9fb9daf7e0a2c4807ac68a0b169aa0b0421b4005
Bob DeBlaey <deblaeyboba@johndeere.com>
2021-10-28 08:11:12 -0500

    first commit on thurs

--- src/components/Random.js
+++ src/components/Random.js
@@ -18,6 +18,24 @@ const Random = (props) => {
             <div>{props.drinkState.strDrink}</div>
             <img src={props.drinkState.strDrinkThumb} alt=""/>
             <div>Instructions: {props.drinkState.strInstructions}</div>
+
+           {/* begin function for finding non-null ingredients */}
+            <div>
+                {(() => {
+                    for (let i=1; i<16; i++){
+                      if ({props.drinkState.strIngredient[i]} != "null"
+                    ? <ul><li>{props.drinkState.strIngredient[i]}</li></ul>
+                    : }  
+                    }
+                }
+                    
+                )}
+
+            </div>
+           {/* end function for finding non-null ingredients */}
+
+
+
             
             <div></div>
             <button onClick={props.newRandomDrink}>New Random Drink</button>

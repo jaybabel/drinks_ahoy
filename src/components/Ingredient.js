b6e7504102858c511e7bf84d995911ea4ecab31e
Bob DeBlaey <deblaeyboba@johndeere.com>
2021-10-29 15:26:57 -0500

    syncing for slight change

--- src/components/Ingredient.js
+++ src/components/Ingredient.js
@@ -4,63 +4,50 @@ import { Link, Route } from 'react-router-dom';
 import Cocktail from './Cocktail';
 
 class Ingredient extends Component {
-    constructor() {
-        super();
+  constructor() {
+    super();
 
-        this.state = {
-            ingredients: [],
-            searchIngredients: "",
-            searchIngredient: "" ,
-        }
+    this.state = {
+      ingredients: [],
+      searchIngredients: "",
+      searchIngredient: "",
     }
-// Call to get the list of available ingredients and then populate a select input type
-    componentDidMount = () => {
-      axios
-          .get("https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
-          , {
+  }
+  // Call to get the list of available ingredients and then populate a select input type
+  componentDidMount = () => {
+    axios
+      .get("https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+        , {
 
-          })
-          .then((response) => {
-            console.log (response)
-              this.setState({ ingredients: response.data.drinks })
-          })
+        })
+      .then((response) => {
+        console.log(response)
+        this.setState({ ingredients: response.data.drinks })
+      })
   }
 
   render() {
     return (
-        <div>
-            <h1>Ingredient Search</h1>
-            <div className="ingredientList">
-                      <div>
-                       <form>
-                         <select name="ingredients_list" id="ingredients_list">
-                    {this.state.ingredients.map((ingredient,index) => {
-                      return (
-                        console.log(ingredient.strIngredient1),
-                       
-                            <option key={ingredient.id} value={ingredient.strIngredient1}>{ingredient.strIngredient1}</option>
-                       
-                        )}
-                        )}
-                        </select>
-                       </form>
-                       
-                        {/* <Link className="drinkLink" to={`/ingredient/${index}`}>
-                            <div className="thumbnailDiv" key={ingredient.id}
-                                style={{ backgroundImage: `url(${ingredient.strDrinkThumb})`}}>
-                                <p>{ingredient.strDrink}</p>
-                            </div> 
-                        </Link>  */}
+      <div>
+        <h1>Ingredient Search</h1>
+        <div className="ingredientList">
+          <div>
+            <form>
+              <select name="ingredients_list" id="ingredients_list">
+                {this.state.ingredients.map((ingredient, index) => {
+                  return (
+                    console.log(ingredient.strIngredient1),
 
-                        {/* <Cocktail
-                            props={this.state}
-                            /> */}
-                            </div>
-                            )
-                        }
-                    )}
-                </div>
+                    <option key={ingredient.id} value={ingredient.strIngredient1}>{ingredient.strIngredient1}</option>
+
+                  )
+                }
+                )}
+              </select>
+            </form>
+          </div>
         </div>
+      </div>
     )
   }
 }

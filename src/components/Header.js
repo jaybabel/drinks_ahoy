fc7336cd87ebc01f039b4144e7d7bd15a0a02f6b
Bob DeBlaey <deblaeyboba@johndeere.com>
2021-11-01 15:27:56 -0500

    lots of bootswatch CSS applied to Header and Random

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,30 +1,62 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import 'bootswatch/dist/cosmo/bootstrap.min.css'; // Added this :boom:
+import 'bootswatch/dist/darkly/bootstrap.min.css'; // Added this :boom:
 
 import "./Header.css"  
 class Header extends Component {
 
   render() {
     return (
-      <header className="header">
-        <div className="header-left">
-          <h1 className="App-title">Drinks Ahoy!!</h1>
-        </div>    
-        <div className="header-right">   
-        <Link to="/alphabetical" >
-          <div className="alphabeticalLink">
-            <h4>Browse The List</h4>
-          </div>
-        </Link>
+      <div>
+      <nav className="header">
+        <div className="container-fluid">
 
-        <Link to="/ingredient" >
-          <div className="ingredientLink">
-            <h4>Browse by Ingredient</h4>
-          </div>
-        </Link>
+        <ul className="navbar-nav list-group-horizontal">
+
+        <div className="header-left">
+        <h1 className="navbar-brand">
+        <Link to="/" >Drinks Ahoy!!</Link></h1>
         </div>
-      </header>
+       
+        {/* <button className="navbar-toggler" type ="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarColor01"> */}
+        {/* <ul className="navbar-nav list-group-horizontal"> */}
+
+        <li className="nav-item">
+        <div className="nav-link justify-content-right">
+        <Link to="/alphabetical" >Browse The List</Link>
+        </div>
+        </li>
+        <li> </li>
+
+        <li className="nav-item">
+        <div className="nav-link justify-content-right"> 
+        <Link to="/ingredient" >Browse by Ingredient</Link>
+        </div>
+        </li>
+        </ul>
+
+
+        {/* <div className="header-right">
+        <Link to="/alphabetical" >
+        <div className="alphabeticalLink">
+        <h4>Browse The List</h4>
+        </div>
+        </Link> */}
+
+
+        {/* <Link to="/ingredient" >
+        <div className="ingredientLink">
+        <h4>Browse by Ingredient</h4>
+        </div>
+        </Link> */}
+          {/* </div> */}
+        </div>
+      </nav>
+      </div>
     );
   }
 }
